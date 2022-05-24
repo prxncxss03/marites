b26bed26f1d752aa5c3b6c0a7cb5d3be65413f40
prxncxss03 <pocon.evangeline@gmail.com>
2022-05-24 11:29:33 +0800

    add ellipsis

--- js/indivpost.js
+++ js/indivpost.js
@@ -7,6 +7,27 @@ captionMsg.forEach(msg => {
             let result = str.slice(0,320);
             msg.innerHTML = result;
 
+            let addEllipsis = document.createTextNode("...");
+            msg.append(addEllipsis);
+            let moreBtn = document.createElement("button");
+            msg.append(moreBtn);
+            moreBtn.innerText = "more";
+            moreBtn.classList.add("more-btn");
+
+            moreBtn.addEventListener('click', ()=> {
+
+                msg.innerHTML = str;
+                moreBtn.style.display = 'none';
+            })
+            
+            
+        } 
+        else if (str.length > 500) {
+            let result = str.slice(0,500);
+            msg.innerHTML = result;
+
+            let addEllipsis = document.createTextNode("...");
+            msg.append(addEllipsis);
             let moreBtn = document.createElement("button");
             msg.append(moreBtn);
             moreBtn.innerText = "more";
