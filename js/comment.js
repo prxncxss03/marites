26755f0439f4efd9ee3b1bc19799a50d5b6df9ea
Princess Pocon <pocon.evangeline@gmail.com>
2022-05-27 13:49:57 +0800

    add overflow

--- js/comment.js
+++ js/comment.js
@@ -1,22 +1,23 @@
 const commentReax = document.querySelectorAll('.comment-reaction');
 const pop = document.querySelector('.pop-up-content-wrapper');
 const pbutton = document.querySelector('.pop-up-btn-exit');
-const body = document.getElementById("body");
+const body = document.querySelector(".body");
 
 commentReax.forEach(comment => {
     comment.addEventListener("click",
     ()=> {
         pop.style.display = "flex";
         
-        
         disableScroll()
         let pbuttonIsClicked = false;
+        body.style.overflow = "hidden";
 
         pbutton.addEventListener('click', ()=> {
             pbuttonIsClicked = true;
 
             if (pbuttonIsClicked) {
                 pop.style.display = "none";
+                body.style.overflow = "auto";
                 enableScroll();
             }
         })
--- js/createpost.js
+++ js/createpost.js
@@ -1,11 +1,14 @@
 const createPost = document.querySelector('.create-post-btn');
 const createPostPop = document.querySelector('.pop-up-create-post-wrapper');
 const createExit = document.querySelector('.create-pop-up-btn-exit');
+const body2 = document.querySelector(".body");
 
 createPost.addEventListener('click', ()=> {
     createPostPop.style.display = "flex";
+    body2.style.overflow = "hidden";
 
     createExit.addEventListener('click', ()=> {
         createPostPop.style.display = "none";
+        body2.style.overflow = "auto";
     })
 })
